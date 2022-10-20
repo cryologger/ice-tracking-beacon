1c6252251cb79ddc0399a3888b842803499f58bc
Adam <adam.garbo@gmail.com>
2022-10-20 14:19:21 -0400

    Add ADC calibration and float mapping functionality

--- Software/cryologger_itb/03_adc.ino
+++ Software/cryologger_itb/03_adc.ino
@@ -17,9 +17,25 @@ void configureAdc()
   while (ADC->STATUS.bit.SYNCBUSY);               // Wait for synchronization
 
   // Apply ADC gain and offset error calibration correction
-  // Two point calibration at 0.5V & 2.5V (VIN = 5.5V & 27.5)
-  ADC->OFFSETCORR.reg = ADC_OFFSETCORR_OFFSETCORR(29);
-  ADC->GAINCORR.reg = ADC_GAINCORR_GAINCORR(2102);
+  ADC->OFFSETCORR.reg = ADC_OFFSETCORR_OFFSETCORR(-2);
+  ADC->GAINCORR.reg = ADC_GAINCORR_GAINCORR(2049);
   ADC->CTRLB.bit.CORREN = true;
   while (ADC->STATUS.bit.SYNCBUSY); // Wait for synchronization
+}
+
+// Map raw ADC values to floats
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// Calibrate ADC
+void calibrateAdc()
+{
+  float sensorValue = analogRead(PIN_VBAT);
+  float voltage1 = sensorValue * (3.3 / 4095.0);
+  float voltage2 = sensorValue * ((10000000.0 + 1000000.0) / 1000000.0); // Multiply back 1 MOhm / (10 MOhm + 1 MOhm)
+  voltage2 *= 3.3;   // Multiply by 3.3V reference voltage
+  voltage2 /= 4096;  // Convert to voltage
+  Serial.print(F("sensorValue: ")); Serial.print(sensorValue); Serial.print(F(",")); Serial.print(voltage1, 4); Serial.print(F(",")); Serial.println(voltage2, 4);
 }
