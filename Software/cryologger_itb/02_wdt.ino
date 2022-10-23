09550f03b64d269280f5fb04ed7f77b07e1d1333
Adam <adam.garbo@gmail.com>
2022-10-23 07:50:44 -0400

    Code commenting and cleaning
    Standardized WDT code.

--- Software/cryologger_itb/02_wdt.ino
+++ Software/cryologger_itb/02_wdt.ino
@@ -1,8 +1,24 @@
-// Configure the Watchdog Timer to perform a system reset if loop() blocks for more than 8-16 seconds
+// Configure the Watchdog Timer (WDT)
+// Perform a system reset if loop() blocks for more than 8-16 seconds
 void configureWdt()
 {
+  /*
+  // The following code block is used to configure the WDT 
+  // only when using an external real-time clock (RTC) 
+  // (i.e., when GCLK2 is not preconfigured by the internal RTC) 
+  // Configure Generic Clock Controller 2 (GCLK1) for use with the Watchdog Timer (WDT) at 1.024kHz
+  REG_GCLK_GENDIV = GCLK_GENDIV_ID(2) |           // Select Generic Clock Controller 2
+                  GCLK_GENDIV_DIV(4) ;          // Divide the 32.768kHz clock source by divisor 32, where 2^(4 + 1): 32.768kHz/32=1.024kHz
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
 
-  // Feed GCLK2 (preconfigured by the RTC) to WDT
+  REG_GCLK_GENCTRL = GCLK_GENCTRL_ID(2) |         // Select GCLK2
+                   GCLK_GENCTRL_GENEN |         // Enable GCLK2
+                   GCLK_GENCTRL_SRC_OSCULP32K | // Set the clock source to the ultra low power oscillator (OSCULP32K)
+                   GCLK_GENCTRL_DIVSEL |        // Set to divide by 2^(GCLK_GENDIV_DIV(4) + 1)
+                   GCLK_GENCTRL_IDC;            // Set the duty cycle to 50/50 HIGH/LOW (REQUIRED?)
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization to complete
+  */
+  // Feed GCLK2 to WDT (preconfigured by the RTC)
   REG_GCLK_CLKCTRL = GCLK_CLKCTRL_ID_WDT |        // Identify it as the WDT clock
                      GCLK_CLKCTRL_CLKEN |         // Enable the generic clock by setting the Clock Enabler bit
                      GCLK_CLKCTRL_GEN_GCLK2;      // Select GCLK2
@@ -21,33 +37,33 @@ void configureWdt()
   NVIC_EnableIRQ(WDT_IRQn);
 }
 
-// Reset the Watchdog Timer
+// Reset the WDT
 void petDog()
 {
-  //DEBUG_PRINT("Watchdog interrupt: "); DEBUG_PRINTLN(wdtCounter);
-  WDT->CLEAR.bit.CLEAR = 0xA5;        // Clear the Watchdog Timer and restart time-out period //REG_WDT_CLEAR = WDT_CLEAR_CLEAR_KEY;
-  while (WDT->STATUS.bit.SYNCBUSY);   // Await synchronization of registers between clock domains
-  wdtFlag = false;                    // Clear watchdog flag
-  wdtCounter = 0;                     // Reset watchdog interrupt counter
+  //DEBUG_PRINT("WDT interrupt: "); DEBUG_PRINTLN(wdtCounter);
+  REG_WDT_CLEAR = WDT_CLEAR_CLEAR_KEY;  // Clear the WDT and restart time-out period // WDT->CLEAR.bit.CLEAR = 0xA5;
+  while (WDT->STATUS.bit.SYNCBUSY);     // Await synchronization of registers between clock domains
+  wdtFlag = false;                      // Clear WDT flag
+  wdtCounter = 0;                       // Reset WDT interrupt counter
 }
 
-// Watchdog Timer interrupt service routine
+// WDT ISR
 void WDT_Handler()
 {
-  WDT->INTFLAG.bit.EW = 1;            // Clear the Early Warning interrupt flag //REG_WDT_INTFLAG = WDT_INTFLAG_EW;
+  REG_WDT_INTFLAG = WDT_INTFLAG_EW;   // Clear the Early Warning interrupt flag //WDT->INTFLAG.bit.EW = 1;
 
-  // Perform system reset after 10 watchdog interrupts (should not occur)
+  // Perform system reset after 10 WDT interrupts (should not occur)
   if (wdtCounter < 10)
   {
-    WDT->CLEAR.bit.CLEAR = 0xA5;      // Clear the Watchdog Timer and restart time-out period //REG_WDT_CLEAR = WDT_CLEAR_CLEAR_KEY;
-    while (WDT->STATUS.bit.SYNCBUSY); // Await synchronization of registers between clock domains
+    REG_WDT_CLEAR = WDT_CLEAR_CLEAR_KEY;  // Clear the WDT and restart time-out period //WDT->CLEAR.bit.CLEAR = 0xA5;
+    while (WDT->STATUS.bit.SYNCBUSY);     // Await synchronization of registers between clock domains
   }
   else
   {
-    //WDT->CTRL.bit.ENABLE = 0;         // Debugging only: Disable Watchdog
-    //digitalWrite(LED_BUILTIN, HIGH);  // Debugging only: Turn on LED to indicate Watchdog trigger
-    while (true);                     // Force Watchdog Timer to reset the system
+    //WDT->CTRL.bit.ENABLE = 0;         // Debugging only: Disable WDT
+    //digitalWrite(LED_BUILTIN, HIGH);  // Debugging only: Turn on LED to indicate WDT trigger
+    while (true);                     // Force WDT to reset the system
   }
-  wdtFlag = true; // Set the watchdog flag
-  wdtCounter++; // Increment watchdog interrupt counter
+  wdtFlag = true; // Set the WDT flag
+  wdtCounter++; // Increment WDT interrupt counter
 }
