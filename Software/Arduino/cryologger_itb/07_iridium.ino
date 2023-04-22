1586082a041ae96360f82eea5434fcd9b096cc68
Adam <adam.garbo@gmail.com>
2023-04-22 16:08:57 -0400

    Improve setSleepPin function

--- Software/Arduino/cryologger_itb/07_iridium.ino
+++ Software/Arduino/cryologger_itb/07_iridium.ino
@@ -224,14 +224,12 @@ void IridiumSBD::setSleepPin(uint8_t enable)
 {
    if (enable == HIGH)
   {
-      pinMode(this->sleepPin, OUTPUT); // LOW = awake
-      digitalWrite(this->sleepPin, LOW);
-      diagprint(F("HIGH\r\n"));
+      digitalWrite(this->sleepPin, LOW);  // LOW = awake. Inverted by N-MOSFET
+      diagprint(F("AWAKE\r\n"));
    }
    else
    {
-      pinMode(this->sleepPin, OUTPUT);
-      digitalWrite(this->sleepPin, HIGH); // LOW = asleep
-      diagprint(F("LOW\r\n"));
+      digitalWrite(this->sleepPin, HIGH); // HIGH = asleep. Inverted by N-MOSFET
+      diagprint(F("ASLEEP\r\n"));
    }
 }
